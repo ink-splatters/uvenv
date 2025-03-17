046dc7eb8290f0ad0e3969e53301b734a82e9552
Robin van der Noord <robinvandernoord@gmail.com>
2025-03-17 10:26:55 +0100

    build: include `musl` targets (aarch64 and x86_64)
    Fixes #4

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ rm -rf target/wheels/
 # use --zig for better cross compatibility on linux
 maturin build --release --strip --target aarch64-unknown-linux-gnu --zig
 maturin build --release --strip --target x86_64-unknown-linux-gnu --zig
+maturin build --release --strip --target aarch64-unknown-linux-musl --zig
+maturin build --release --strip --target x86_64-unknown-linux-musl --zig
 
 # without zig because that breaks stuff on mac
 maturin build --release --strip --target aarch64-apple-darwin # --zig
